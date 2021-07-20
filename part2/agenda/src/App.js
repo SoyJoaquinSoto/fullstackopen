247d9cfa091a5587573e7508dbff555dc04c677c
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-07-20 13:17:38 -0500

    Entrega de ejercicio 2.6

---
+++ part2/agenda/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const App = () => {
+	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [newName, setNewName] = useState("");
+
+	const handleChange = (e) => {
+		setNewName(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		setPersons([...persons, { name: newName }]);
+	};
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					name: <input value={newName} onChange={handleChange} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			<div>
+				{persons &&
+					persons.map((person) => <p key={person.name}>{person.name}</p>)}
+			</div>
+		</div>
+	);
+};
+
+export default App;
