03bd07f93148228981cdb7504a0aab7de313e66a
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-07-20 09:47:59 -0500

    Entrega de ejercicio 2.5

--- part2/informacion/src/index.js
+++ part2/informacion/src/index.js
@@ -1,45 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = ({ course }) => {
-	return <h2>{course.name}</h2>;
-};
-
-const Total = ({ course }) => {
-	const sum = course.parts.reduce((acum, part) => acum + part.exercises, 0);
-	return (
-		<p>
-			<b>Total of {sum} exercises</b>
-		</p>
-	);
-};
-
-const Part = ({ part }) => {
-	return (
-		<p>
-			{part.name} {part.exercises}
-		</p>
-	);
-};
-
-const Content = ({ course }) => {
-	return (
-		<div>
-			{course.parts &&
-				course.parts.map((part) => <Part part={part} key={part.id} />)}
-		</div>
-	);
-};
-
-const Course = ({ course }) => {
-	return (
-		<div>
-			<Header course={course} />
-			<Content course={course} />
-			<Total course={course} />
-		</div>
-	);
-};
+import Course from "./Course";
 
 const App = () => {
 	const courses = [
