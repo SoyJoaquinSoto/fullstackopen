24d7825c8c1635489a67b40ce1fee150e292281e
SoyJoaquinSoto <jsoto1800@alumno.ipn.mx>
2021-07-20 13:31:09 -0500

    Entrega de ejercicio 2.9

--- part2/agenda/src/App.js
+++ part2/agenda/src/App.js
@@ -1,30 +1,58 @@
 import React, { useState } from "react";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "040-123456" },
+		{ name: "Ada Lovelace", number: "39-44-5323523" },
+		{ name: "Dan Abramov", number: "12-43-234345" },
+		{ name: "Mary Poppendieck", number: "39-23-6423122" },
+	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
+	const [nameFilter, setNameFilter] = useState("");
 
-	const handleChange = (e) => {
+	const handleNameChange = (e) => {
 		setNewName(e.target.value);
+	};
+
+	const handleNumberChange = (e) => {
+		setNewNumber(e.target.value);
+	};
+
+	const handleNameFilterChange = (e) => {
+		setNameFilter(e.target.value);
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+
+		if (!newName || !newNumber) {
+			return;
+		}
 
 		if (persons.find((person) => person.name === newName)) {
 			alert(`${newName} is already added to phonebook`);
 			return;
 		}
 
-		setPersons([...persons, { name: newName }]);
+		setPersons([...persons, { name: newName, number: newNumber }]);
 	};
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				filter shown with:
+				<input value={nameFilter} onChange={handleNameFilterChange} />
+			</div>
+
+			<h2>add a new</h2>
 			<form onSubmit={handleSubmit}>
 				<div>
-					name: <input value={newName} onChange={handleChange} />
+					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					number: <input value={newNumber} onChange={handleNumberChange} />
 				</div>
 				<div>
 					<button type="submit">add</button>
@@ -33,7 +61,15 @@ const App = () => {
 			<h2>Numbers</h2>
 			<div>
 				{persons &&
-					persons.map((person) => <p key={person.name}>{person.name}</p>)}
+					persons
+						.filter((person) =>
+							person.name.toUpperCase().includes(nameFilter.toUpperCase())
+						)
+						.map((person) => (
+							<p key={person.name}>
+								{person.name} {person.number}
+							</p>
+						))}
 			</div>
 		</div>
 	);
